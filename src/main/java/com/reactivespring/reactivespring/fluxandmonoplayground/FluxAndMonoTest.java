6c00476596abbc81b4506b922fab8d919c50e23c
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-11-26 10:08:30 -0300

    flux: subscribe and erro

---
+++ src/main/java/com/reactivespring/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -0,0 +1,21 @@
+package com.reactivespring.reactivespring.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest(){
+
+            Flux<String> stringFlux  = Flux.just("Data01", "Data02", "Data03")
+                    .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                    .concatWith(Flux.just("After Error"))
+                    .log();
+
+        stringFlux
+                .subscribe(System.out::println,
+                        (e) -> System.err.println("Exception is" + e)
+                ,() -> System.out.println("completed"));
+    }
+}
