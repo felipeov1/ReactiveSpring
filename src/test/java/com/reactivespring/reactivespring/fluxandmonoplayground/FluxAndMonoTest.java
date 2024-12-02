2d66264e1641d31b7db09d71d1031b22d618b765
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-12-02 09:46:15 -0300

    feat: Mono Test

--- src/test/java/com/reactivespring/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/reactivespring/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,8 @@ package com.reactivespring.reactivespring.fluxandmonoplayground;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -25,8 +27,74 @@ public class FluxAndMonoTest {
         Flux<String> stringFlux = Flux.just("Data01", "Data02", "Data03")
             .log();
 
-        StepVerifier.create(stringFlux);
-
-
+        StepVerifier.create(stringFlux)
+                .expectNext("Data01")
+                .expectNext("Data02")
+                .expectNext("Data03")
+                .verifyComplete();
     }
+
+    @Test
+    public void fluxTestElements_WithError1(){
+
+        Flux<String> stringFlux = Flux.just("Data01", "Data02", "Data03")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Data01","Data02", "Data03")
+                .expectErrorMessage("Exception Occurred")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElements_WithError2(){
+
+        Flux<String> stringFlux = Flux.just("Data01", "Data02", "Data03")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Data01")
+                .expectNext("Data02")
+                .expectNext("Data03")
+//                .expectError(RuntimeException.class)
+                .expectErrorMessage("Exception Occurred")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElementsCount_WithError3(){
+
+        Flux<String> stringFlux = Flux.just("Data01", "Data02", "Data03")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectErrorMessage("Exception Occurred")
+                .verify();
+    }
+
+    @Test
+    public void monoTest(){
+        Mono<String>  stringMono = Mono.just("Spring");
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoTest_Error(){
+        Mono<String> stringMono = Mono.just("String");
+
+        StepVerifier.create(Mono.error(new RuntimeException("Exception Ocurred")).log())
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+
+
+
 }
